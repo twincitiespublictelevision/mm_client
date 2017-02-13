a4239d7812a07c60090ef9252d52b6f002d95add
Augustus Mayo <gusmayo@gmail.com>
2017-02-13 00:01:37 -0600

    Update to staging API. Add tests. Add docs.

--- src/client.rs
+++ src/client.rs
@@ -18,13 +18,15 @@ use error::MMCResult;
 #[cfg(not(test))]
 const LIVE_URL: &'static str = "https://media.services.pbs.org/api/v1";
 #[cfg(not(test))]
-const QA_URL: &'static str = "https://media-staging.services.pbs.org/api/v1";
+const STAGING_URL: &'static str = "https://media-staging.services.pbs.org/api/v1";
 
 #[cfg(test)]
 const LIVE_URL: &'static str = mockito::SERVER_URL;
-#[cfg(test)]
-const QA_URL: &'static str = mockito::SERVER_URL;
+#[cfg(tet)]
+const STAGING_URL: &'static str = mockito::SERVER_URL;
 
+/// A client for communicating with the Media Manager API
+#[derive(Debug)]
 pub struct Client {
     key: String,
     secret: String,
@@ -34,15 +36,31 @@ pub struct Client {
 
 pub type Params<'a> = Vec<(&'a str, &'a str)>;
 
-#[derive(Debug)]
+/// The Media Manager endpoints that are supported by [Client](struct.Client.html)
+#[derive(Clone, Debug)]
 pub enum Endpoints {
+    /// Represents the assets endpoint
     Asset,
+
+    /// Represents the changelog endpoint
     Changelog,
+
+    /// Represents the collections endpoint
     Collection,
+
+    /// Represents the episodes endpoint
     Episode,
+
+    /// Represents the franchises endpoint
     Franchise,
+
+    /// Represents the seasons endpoint
     Season,
+
+    /// Represents the shows endpoint
     Show,
+
+    /// Represents the specials endpoint
     Special,
 }
 
@@ -89,12 +107,14 @@ impl str::FromStr for Endpoints {
 }
 
 impl Client {
+    /// Generates a new client for the production Media Manager API
     pub fn new(key: &str, secret: &str) -> MMCResult<Client> {
         Client::client_builder(key, secret, LIVE_URL)
     }
 
-    pub fn qa(key: &str, secret: &str) -> MMCResult<Client> {
-        Client::client_builder(key, secret, QA_URL)
+    /// Generates a new client for the staging Media Manager API
+    pub fn staging(key: &str, secret: &str) -> MMCResult<Client> {
+        Client::client_builder(key, secret, STAGING_URL)
     }
 
     fn client_builder(key: &str, secret: &str, base: &str) -> MMCResult<Client> {
@@ -108,80 +128,81 @@ impl Client {
         })
     }
 
+    /// Attempts to fetch a single object with the requested id from the requested
+    /// Media Manager API endpoint
     pub fn get(&self, endpoint: Endpoints, id: &str) -> MMCResult<String> {
-        self.rq_get(vec![self.base.as_str(), "/", endpoint.to_string().as_str(), "/", id, "/"]
-                        .join("")
-                        .as_str(),
-                    vec![])
+        self.rq_get(Client::build_url(self.base.as_str(), endpoint, Some(id), vec![]).as_str())
     }
 
+    /// Attempts to fetch a list of objects from the requested Media Manager API endpoint augmented
+    /// by the requested parameters
     pub fn list(&self, endpoint: Endpoints, params: Params) -> MMCResult<String> {
-        self.rq_get(vec![self.base.as_str(), "/", endpoint.to_string().as_str(), "/"]
-                        .join("")
-                        .as_str(),
-                    params)
+        self.rq_get(Client::build_url(self.base.as_str(), endpoint, None, params).as_str())
     }
 
+    /// Allows for calling any arbitrary url from the Media Manager API
     pub fn url(&self, url: &str) -> MMCResult<String> {
-        self.rq_get(url, vec![])
+        self.rq_get(url)
     }
 
+    /// Shorthand for accessing a single asset
     pub fn asset(&self, id: &str) -> MMCResult<String> {
         self.get(Endpoints::Episode, id)
     }
 
+    /// Shorthand for accessing a list of changes
     pub fn changelog(&self, params: Params) -> MMCResult<String> {
         self.list(Endpoints::Changelog, params)
     }
 
+    /// Shorthand for accessing a single collection
     pub fn collection(&self, id: &str) -> MMCResult<String> {
         self.get(Endpoints::Collection, id)
     }
 
+    /// Shorthand for accessing a list of collections
     pub fn collections(&self, params: Params) -> MMCResult<String> {
         self.list(Endpoints::Collection, params)
     }
 
+    /// Shorthand for accessing a single episode
     pub fn episode(&self, id: &str) -> MMCResult<String> {
         self.get(Endpoints::Episode, id)
     }
 
+    /// Shorthand for accessing a single franchise
     pub fn franchise(&self, id: &str) -> MMCResult<String> {
         self.get(Endpoints::Franchise, id)
     }
 
+    /// Shorthand for accessing a list of franchises
     pub fn franchises(&self, params: Params) -> MMCResult<String> {
         self.list(Endpoints::Franchise, params)
     }
 
+    /// Shorthand for accessing a single season
     pub fn season(&self, id: &str) -> MMCResult<String> {
         self.get(Endpoints::Season, id)
     }
 
+    /// Shorthand for accessing a single special
     pub fn special(&self, id: &str) -> MMCResult<String> {
         self.get(Endpoints::Special, id)
     }
 
+    /// Shorthand for accessing a single show
     pub fn show(&self, id: &str) -> MMCResult<String> {
         self.get(Endpoints::Show, id)
     }
 
+    /// Shorthand for accessing a list of shows
     pub fn shows(&self, params: Params) -> MMCResult<String> {
         self.list(Endpoints::Show, params)
     }
 
-    fn rq_get(&self, base_url: &str, params: Params) -> MMCResult<String> {
-        let mut param_string = params.iter()
-            .map(|&(name, value)| format!("{}={}", name, value))
-            .collect::<Vec<String>>()
-            .join("&");
-
-        if !params.is_empty() {
-            param_string = "?".to_owned() + param_string.as_str();
-        }
-
+    fn rq_get(&self, url: &str) -> MMCResult<String> {
         self.client
-            .get(format!("{}{}", base_url, param_string).as_str())
+            .get(url)
             .header(Authorization(Basic {
                 username: self.key.to_string(),
                 password: Some(self.secret.to_string()),
@@ -189,13 +210,49 @@ impl Client {
             .header(Connection::close())
             .send()
             .map_err(MMCError::Network)
-            .and_then(|response| self.handle_response(response))
+            .and_then(|response| Client::handle_response(response))
     }
 
-    fn handle_response(&self, response: Response) -> MMCResult<String> {
+    fn build_url(base_url: &str, endpoint: Endpoints, id: Option<&str>, params: Params) -> String {
+
+        // Create the new base for the returned url
+        let mut url = base_url.to_string();
+        url.push('/');
+
+        // Parse the requested endpoint
+        let endpoint_string = endpoint.to_string();
+        url.push_str(endpoint_string.as_str());
+        url.push('/');
+
+        // Optional add the id if it was supplied
+        if let Some(id_val) = id {
+            url.push_str(id_val);
+            url.push('/');
+        }
+
+        // Add the query parameters to the url
+        url + Client::format_params(params).as_str()
+    }
+
+    fn format_params(params: Params) -> String {
+        if !params.is_empty() {
+            let param_string = params.iter()
+                .map(|&(name, value)| format!("{}={}", name, value))
+                .collect::<Vec<String>>()
+                .join("&");
+
+            let mut args = "?".to_owned();
+            args.push_str(param_string.as_str());
+            args
+        } else {
+            String::new()
+        }
+    }
+
+    fn handle_response(response: Response) -> MMCResult<String> {
         match *response.status() {
-            StatusCode::Ok => self.parse_success(response),
-            StatusCode::BadRequest => self.parse_bad_request(response),
+            StatusCode::Ok => Client::parse_success(response),
+            StatusCode::BadRequest => Client::parse_bad_request(response),
             StatusCode::Unauthorized => Err(MMCError::NotAuthorized),
             StatusCode::Forbidden => Err(MMCError::NotAuthorized),
             StatusCode::NotFound => Err(MMCError::ResourceNotFound),
@@ -203,15 +260,15 @@ impl Client {
         }
     }
 
-    fn parse_success(&self, response: Response) -> MMCResult<String> {
-        self.parse_response_body(response)
+    fn parse_success(response: Response) -> MMCResult<String> {
+        Client::parse_response_body(response)
     }
 
-    fn parse_bad_request(&self, response: Response) -> MMCResult<String> {
-        self.parse_response_body(response).and_then(|body| Err(MMCError::BadRequest(body)))
+    fn parse_bad_request(response: Response) -> MMCResult<String> {
+        Client::parse_response_body(response).and_then(|body| Err(MMCError::BadRequest(body)))
     }
 
-    fn parse_response_body(&self, mut response: Response) -> MMCResult<String> {
+    fn parse_response_body(mut response: Response) -> MMCResult<String> {
 
         // Create a buffer to read the response stream into
         let mut buffer = Vec::new();
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,53 @@
+//! # mm_client
+//!
+//! The `mm_client` crate is a very small library for communicating with the PBS Media Manager API
+//! easier. It provides a [Client](struct.Client.html) for querying against either the production
+//! API or the staging API.
+//!
+//! The main goals of the crate are to:
+//! * Provide authentication handling
+//! * Manage API url construction
+//! * Handle API error responses
+//! * Make few assumptions about how responses will be used
+//!
+//! Currently all requests made by a [Client](struct.Client.html) are synchronous.
+//!
+//! # Creating a [Client](struct.Client.html)
+//!
+//! [Client](struct.Client.html) provides two constructors, one for the accessing the production
+//! API and one for the staging API. Both constructors take an API key and secret as arguments. It
+//! is recommended to create a single [Client](struct.Client.html) that is then passed around for
+//! making requests.
+//!
+//! Note that constructing a client may fail.
+//!
+//! ```no_run
+//! let client = Client::new("API_KEY", "API_SECRET").unwrap();
+//! ```
+//!
+//! # Fetching a single object
+//!
+//! Requesting a single object can be performed by using the `get` method
+//!
+//! ```no_run
+//! let client = Client::new("API_KEY", "API_SECRET").unwrap();
+//! let response = client.get(Endpoints::Asset, "asset-id");
+//! ```
+//! The response string can then be handed off a JSON parser for further use.
+//!
+//! # Fetching a list of objects
+//!
+//! Requesting a list of objects can be performed by using the `list` method
+//!
+//! ```no_run
+//! let client = Client::new("API_KEY", "API_SECRET").unwrap();
+//! let params = vec![("since", "2017-02-12T00:00:00Z")];
+//! let response = client.list(Endpoints::Show, params);
+//! ```
+//! Here a request is made for all of the show objects that have been updated since the supplied
+//! date. Similar to the `get` method, the response string is available to pass to a JSON parser
+
+#![deny(missing_docs)]
 #[macro_use]
 #[cfg(test)]
 extern crate assert_matches;
@@ -31,8 +81,8 @@ mod tests {
     const SECRET: &'static str = "world";
     const BASIC_AUTH: &'static str = "Basic aGVsbG86d29ybGQ=";
 
-    fn sample_client() -> Client<'static> {
-        Client::qa(KEY, SECRET).unwrap()
+    fn sample_client() -> Client {
+        Client::staging(KEY, SECRET).unwrap()
     }
 
     fn show_get(id: &str) -> MMCResult<String> {
@@ -47,18 +97,19 @@ mod tests {
         Uuid::new_v4().hyphenated().to_string()
     }
 
-    fn mock_show_endpoint(id: &str) -> Mock {
-        mock("GET", vec!["/shows/", id, "/"].join("").as_str())
+    fn mock_single(endpoint: &str, id: &str) -> Mock {
+        mock("GET", vec!["/", endpoint, "/", id, "/"].join("").as_str())
     }
 
-    fn mock_shows_endpoint(param_string: &str) -> Mock {
-        mock("GET", vec!["/shows/", param_string].join("").as_str())
+    fn mock_list(endpoint: &str, param_string: &str) -> Mock {
+        mock("GET",
+             vec!["/", endpoint, "/", param_string].join("").as_str())
     }
 
     #[test]
     fn handles_single_200() {
         let id = random_id();
-        let mock = mock_show_endpoint(id.as_str())
+        let mock = mock_single("shows", id.as_str())
             .with_status(200)
             .with_header("content-type", "application/json")
             .with_body("{\"name\":\"value\"}")
@@ -73,7 +124,7 @@ mod tests {
     fn handles_list_200() {
         let param_string = "?param1=value1&param2=value2";
 
-        let mock = mock_shows_endpoint(param_string)
+        let mock = mock_list("shows", param_string)
             .with_status(200)
             .with_header("content-type", "application/json")
             .with_body("{\"name\":\"value\"}")
@@ -91,7 +142,7 @@ mod tests {
         let mut param_string = "?param=".to_string();
         param_string.push_str(id.as_str());
 
-        let mock = mock_shows_endpoint(param_string.as_str())
+        let mock = mock_list("shows", param_string.as_str())
             .match_header("Authorization", BASIC_AUTH)
             .with_status(200)
             .with_header("content-type", "application/json")
@@ -107,7 +158,7 @@ mod tests {
     #[test]
     fn handles_400() {
         let id = random_id();
-        let mock = mock_show_endpoint(id.as_str())
+        let mock = mock_single("shows", id.as_str())
             .with_status(400)
             .with_header("content-type", "text/plain")
             .with_body("Failure message from the server")
@@ -123,7 +174,7 @@ mod tests {
     #[test]
     fn handles_401() {
         let id = random_id();
-        let mock = mock_show_endpoint(id.as_str())
+        let mock = mock_single("shows", id.as_str())
             .with_status(401)
             .create_for(|| assert_matches!(show_get(id.as_str()), Err(MMCError::NotAuthorized)))
             .remove();
@@ -132,7 +183,7 @@ mod tests {
     #[test]
     fn handles_403() {
         let id = random_id();
-        let mock = mock_show_endpoint(id.as_str())
+        let mock = mock_single("shows", id.as_str())
             .with_status(403)
             .create_for(|| assert_matches!(show_get(id.as_str()), Err(MMCError::NotAuthorized)))
             .remove();
@@ -141,7 +192,7 @@ mod tests {
     #[test]
     fn handles_404() {
         let id = random_id();
-        let mock = mock_show_endpoint(id.as_str())
+        let mock = mock_single("shows", id.as_str())
             .with_status(404)
             .create_for(|| assert_matches!(show_get(id.as_str()), Err(MMCError::ResourceNotFound)))
             .remove();
@@ -150,12 +201,64 @@ mod tests {
     #[test]
     fn handles_500() {
         let id = random_id();
-        let mock = mock_show_endpoint(id.as_str())
+        let mock = mock_single("shows", id.as_str())
             .with_status(500)
             .create_for(|| {
                 assert_matches!(show_get(id.as_str()),
                                 Err(MMCError::APIFailure(StatusCode::InternalServerError)))
             })
             .remove();
+    }
+
+    #[test]
+    fn shorthand_singles_ok() {
+        let id = random_id();
+
+        let endpoints = vec![Endpoints::Asset,
+                             Endpoints::Collection,
+                             Endpoints::Episode,
+                             Endpoints::Franchise,
+                             Endpoints::Season,
+                             Endpoints::Special,
+                             Endpoints::Show];
+
+        for endpoint in endpoints.into_iter() {
+            mock_single(endpoint.to_string().as_str(), id.as_str())
+                .with_status(200)
+                .with_header("content-type", "application/json")
+                .with_body("{\"name\":\"value\"}")
+                .create_for(|| {
+                    let test_response = String::from("{\"name\":\"value\"}");
+                    assert_matches!(sample_client().get(endpoint.clone(), id.as_str()),
+                                    Ok(test_response))
+                })
+                .remove();
+        }
+    }
+
+    #[test]
+    fn shorthand_list_ok() {
+        let id = random_id();
+
+        let param_string = vec!["?param1=", id.as_str(), "&param2=value2"].join("");
+        let params = vec![("param1", id.as_str()), ("param2", "value2")];
+
+        let endpoints = vec![Endpoints::Changelog,
+                             Endpoints::Collection,
+                             Endpoints::Franchise,
+                             Endpoints::Show];
+
+        for endpoint in endpoints.into_iter() {
+            mock_list(endpoint.to_string().as_str(), param_string.as_str())
+                .with_status(200)
+                .with_header("content-type", "application/json")
+                .with_body("{\"name\":\"value\"}")
+                .create_for(|| {
+                    let test_response = String::from("{\"name\":\"value\"}");
+                    assert_matches!(sample_client().list(endpoint.clone(), params.clone()),
+                                    Ok(test_response))
+                })
+                .remove();
+        }
     }
 }
